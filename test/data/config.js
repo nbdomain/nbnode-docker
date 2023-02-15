10740f9d01ed52ec3456f2cb24d256916615496b
bloodchen <bloodchen@gmail.com>
2023-02-15 10:54:41 +0800

    update

--- test/data/config.js
+++ test/data/config.js
@@ -10,16 +10,16 @@
 // ------------------------------------------------------------------------------------------------
 const CONFIG = {
   debug: true,
-  key: "", //unique node key. use 'node key -c bitcoin' to generate
+  key: "KyQYxHjX7ZHhNVamhuNs9JgvR7SM5FuMnbverckPLKi2ZG31zy5R", //unique node key. use 'node key -c bitcoin' to generate
   "node_info": { //public info of the node
     nbdomain: "", //nbdomain of the node
     payment: { bsv: "1Bti24c8ZQLYMTaifVkiRwJ1cwT4K6ucJu", ar: "gOyqCZBB-JmX1eDcYrIPPV71msTBBzPKwnEF3oEB-ZQ" }, //address to receive payment
     contact: "", //contact email of the owner,required for ssl certificate
   },
-  chainid: 'main',
+  chainid: 'private',
   adminKey: null, //enable remote admin features, check /api/admin endpoint
   server: {
-    publicUrl: "", //public domain name or IP of the node, must set for public node and not set for private node
+    publicUrl: "http://pi.skyjeff.com:10000", //public domain name or IP of the node, must set for public node and not set for private node
     autoSSL: false, //for auto generated SSL certificate, require publicUrl to be set
     port: 9000,  //local port for http service, https will use 443 and ignore this setting
     hideFromList: false //(optional) if add this node to /api/nodes endpoint
@@ -34,8 +34,9 @@ const CONFIG = {
     }
   },
   allowIPs: [],
+  apiKeys: [],
   dataPath: "", //Existing Disk Path for big data, default is core/db/data
-  pnodes: [],//other(than public) nbnode peers and will not share with other nodes
+  pnodes: ["http://pi.skyjeff.com:9300"],//other(than public) nbnode peers and will not share with other nodes
   "exit_count": 0, //exit the process each x minutes. Used with PM2 to restart process every x minutes. 0 for no exit
 }
 // ------------------------------------------------------------------------------------------------
